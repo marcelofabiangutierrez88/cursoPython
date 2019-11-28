bc381af64714a6133477fb6e0fa9f341a309f1ec
Marcelo Gutierrez <49662472+marcelofabiangutierrez88@users.noreply.github.com>
2019-11-27 21:31:33 -0300

    Add files via upload

---
+++ Ejercicio1_Saludo.py
@@ -0,0 +1,9 @@
+# Escribir un programa que pregunte al usuario su nombre, y luego lo salude.
+def pedirDatos():
+    nom = input("ingrese nombre: ")
+    print("hola que tal,",nom)
+    
+def main():
+    pedirDatos()
+    
+main()
