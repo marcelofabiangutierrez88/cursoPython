8605d414cb54e5dd10250f02681e9f45d9656647
Marcelo Gutierrez <49662472+marcelofabiangutierrez88@users.noreply.github.com>
2019-12-01 11:48:23 -0300

    Add files via upload

---
+++ ejercicio7_convertirMinutos.py
@@ -0,0 +1,8 @@
+def convertirMinutos():
+    minutos = int(input("Ingrese cantidad de minutos: "))
+    hora = minutos/60
+    print("el valor ingresado en minutos es igual a esta cantidad de horas: ",hora, "hora/s")
+def main():
+    convertirMinutos()
+main()
+    
