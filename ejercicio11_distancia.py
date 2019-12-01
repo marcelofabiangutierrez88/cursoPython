b2bb7b915ed6fee08d31090b77fa6054914feb0b
Marcelo Gutierrez <49662472+marcelofabiangutierrez88@users.noreply.github.com>
2019-12-01 11:49:06 -0300

    Add files via upload

---
+++ ejercicio11_distancia.py
@@ -0,0 +1,11 @@
+def distancia():
+    num1 = int(input("Ingrese primer numero:"))
+    num2 = int(input("Ingrese segundo numero:"))
+    distancia = num1-num2
+    abs(distancia)
+    
+    print("Distancia entre numeros: ", distancia)
+
+def main():
+    distancia()
+main()
