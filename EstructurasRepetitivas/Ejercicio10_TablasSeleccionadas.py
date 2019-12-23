d89d0f1a638bb516a8129937729ef694ddf09c9b
Marcelo Gutierrez <49662472+marcelofabiangutierrez88@users.noreply.github.com>
2019-12-23 11:06:25 -0300

    Add files via upload

---
+++ EstructurasRepetitivas/Ejercicio10_TablasSeleccionadas.py
@@ -0,0 +1,18 @@
+#Ejercicio 10
+#Algoritmo que muestre la tabla de multiplicar de los
+#números 1,2,3,4 y 5.
+
+def multi():
+    x = int (input("Ingrese tabla (1/2/3/4/5): "))
+    i =0
+    for x in range (x,10):
+        while i<11 and :
+            tabla =x*i
+            i+=1
+            print(tabla)
+        i+=1
+        
+def main():
+    multi()
+main()
+        
