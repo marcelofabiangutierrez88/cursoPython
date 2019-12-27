9a4bde8e1f7c8744e92f6c5fbec4c99d58b5e909
Marcelo Gutierrez <49662472+marcelofabiangutierrez88@users.noreply.github.com>
2019-12-27 14:35:01 -0300

    Add files via upload

---
+++ Funciones/Ejercicio4_CalculaEspacio.py
@@ -0,0 +1,15 @@
+#Ejercicio 4
+#Crea un función “ConvertirEspaciado”,
+#que reciba como parámetro un texto y
+#devuelve una cadena con un espacio adicional tras cada letra.
+#Por ejemplo,
+#“Hola, tú” devolverá “H o l a , t ú “.
+#Crea un programa principal donde se use dicha función.
+
+#def convertirEspaciado(texto):
+#    r
+
+texto = "Hola"
+for i in texto:
+    i1 = i+ ' '
+    print(i1)
